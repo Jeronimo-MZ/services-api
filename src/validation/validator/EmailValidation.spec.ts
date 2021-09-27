546d406bc1788f01549ae01ef7527eae14e09acd
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-09-27 21:30:26 +0200

    test: ensure EmailValidation calls EmailValidator with correct email

---
+++ src/validation/validator/EmailValidation.spec.ts
@@ -0,0 +1,30 @@
+import faker from "faker";
+
+import { EmailValidatorSpy } from "@/validation/mocks/mockEmailValidator";
+
+import { EmailValidation } from "./EmailValidation";
+
+const field = faker.random.word();
+
+type SutTypes = {
+    sut: EmailValidation;
+    emailValidatorSpy: EmailValidatorSpy;
+};
+
+const makeSut = (): SutTypes => {
+    const emailValidatorSpy = new EmailValidatorSpy();
+    const sut = new EmailValidation(field, emailValidatorSpy);
+    return {
+        sut,
+        emailValidatorSpy,
+    };
+};
+
+describe("Email Validation", () => {
+    it("should call EmailValidator with correct email", () => {
+        const { sut, emailValidatorSpy } = makeSut();
+        const email = faker.internet.email();
+        sut.validate({ [field]: email });
+        expect(emailValidatorSpy.email).toBe(email);
+    });
+});
