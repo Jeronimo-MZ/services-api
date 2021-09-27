537421a4ce2daf39bc1918b4a2c8972d032a1435
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-09-27 20:48:37 +0200

    feat: ensure BcryptAdapter returns a valid hash on hash() success

--- src/infra/cryptography/BcryptAdapter/BcryptAdapter.ts
+++ src/infra/cryptography/BcryptAdapter/BcryptAdapter.ts
@@ -5,8 +5,6 @@ import { Hasher } from "@/data/protocols/cryptography";
 export class BcryptAdapter implements Hasher {
     constructor(private readonly salt: number) {}
     async hash(plaintext: string): Promise<string> {
-        hash(plaintext, this.salt);
-
-        return null as unknown as string;
+        return await hash(plaintext, this.salt);
     }
 }
