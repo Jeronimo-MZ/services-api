56c9ad89de60c12520e81e89eba912bff0ace977
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-09-27 22:56:25 +0200

    feat: ensure MinLengthValidation returns InvalidParamError if validation fails

--- src/validation/validator/MinLengthValidation.ts
+++ src/validation/validator/MinLengthValidation.ts
@@ -4,13 +4,18 @@ import { Validation } from "@/presentation/protocols";
 export class MinLengthValidation implements Validation {
     constructor(
         private readonly field: string,
-        private readonly _minLength: number,
+        private readonly minLength: number,
     ) {}
 
     validate(input: any): Error | null {
         if (typeof input[this.field] !== "string") {
             return new InvalidParamError(this.field);
         }
+
+        if (input[this.field].length < this.minLength) {
+            return new InvalidParamError(this.field);
+        }
+
         return null;
     }
 }
