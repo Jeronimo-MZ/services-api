afdc92c91c141df793c82df51ecab2c9ea9edf01
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-09-27 21:15:55 +0200

    test: ensure RequiredFieldsValidation returns null if validation succeeds

--- src/validation/validator/RequiredFieldValidation.spec.ts
+++ src/validation/validator/RequiredFieldValidation.spec.ts
@@ -16,4 +16,10 @@ describe("RequiredField Validation", () => {
         const error = sut.validate({ invalidField: faker.random.word() });
         expect(error).toEqual(new MissingParamError(field));
     });
+
+    it("should not return if validation succeeds", () => {
+        const sut = makeSut();
+        const error = sut.validate({ [field]: faker.random.word() });
+        expect(error).toBeNull();
+    });
 });
