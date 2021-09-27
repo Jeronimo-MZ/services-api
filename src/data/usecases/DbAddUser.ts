3df6d3c8f976783d940160a75569394a0b453a06
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-09-27 15:43:58 +0200

    feat: ensure DbAddUser returns an account on success

--- src/data/usecases/DbAddUser.ts
+++ src/data/usecases/DbAddUser.ts
@@ -12,14 +12,15 @@ export class DbAddUser implements AddUser {
         private readonly addUserRepository: AddUserRepository,
     ) {}
     async add({ name, password, email }: AddUserParams): Promise<User | null> {
+        const user = await this.loadUserByEmailRepository.loadByEmail(email);
+        if (user) {
+            return null;
+        }
         const hashed_password = await this.hasher.hash(password);
-
-        await this.loadUserByEmailRepository.loadByEmail(email);
-        await this.addUserRepository.add({
+        return await this.addUserRepository.add({
             email,
             name,
             password: hashed_password,
         });
-        return null;
     }
 }
