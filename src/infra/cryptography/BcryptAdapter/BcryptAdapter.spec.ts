03c7cd323dcc9720d7ec4ee9babfc63f9c999998
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-09-27 20:49:00 +0200

    test: ensure BcryptAdapter returns a valid hash on hash() success

--- src/infra/cryptography/BcryptAdapter/BcryptAdapter.spec.ts
+++ src/infra/cryptography/BcryptAdapter/BcryptAdapter.spec.ts
@@ -4,11 +4,11 @@ import faker from "faker";
 import { BcryptAdapter } from "./BcryptAdapter";
 
 const salt = 12;
-const hash = faker.random.alphaNumeric(30);
+const generatedHash = faker.random.alphaNumeric(30);
 
 jest.mock("bcrypt", () => ({
     hash: async (): Promise<string> => {
-        return hash;
+        return generatedHash;
     },
 }));
 const makeSut = (): BcryptAdapter => new BcryptAdapter(salt);
@@ -21,6 +21,12 @@ describe("BcryptAdapter", () => {
             const plaintext = faker.internet.password();
             await sut.hash(plaintext);
             expect(hashSpy).toHaveBeenCalledWith(plaintext, salt);
+        });
+
+        it("should return a valid hash on hash success", async () => {
+            const sut = makeSut();
+            const hash = await sut.hash(faker.internet.password());
+            expect(hash).toBe(generatedHash);
         });
     });
 });
