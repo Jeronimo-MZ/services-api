75665de012f6245f4c28d0c90ab7dceb1266a27e
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-09-28 16:23:16 +0200

    test: ensure cors is enabled

---
+++ src/main/middlewares/cors.spec.ts
@@ -0,0 +1,20 @@
+import request from "supertest";
+
+import { app } from "@/main/config/app";
+
+import { cors } from "./cors";
+
+describe("CORS middleware", () => {
+    it("should enable CORS", async () => {
+        app.get("/test_cors", cors, (_request, response) => {
+            return response.send();
+        });
+
+        await request(app)
+            .get("/test_cors")
+            .send()
+            .expect("access-control-allow-origin", "*")
+            .expect("access-control-allow-methods", "*")
+            .expect("access-control-allow-headers", "*");
+    });
+});
