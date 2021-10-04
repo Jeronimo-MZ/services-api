3a52b64eeab18c6fe0ea03cfe2ff22ddc3f0a846
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-10-04 21:07:55 +0200

    test: ensure DbAuthentication returns null if LoadUserByEmailRepository returns null

--- src/data/usecases/Authentication/DbAuthentication.spec.ts
+++ src/data/usecases/Authentication/DbAuthentication.spec.ts
@@ -4,11 +4,22 @@ import { LoadUserByEmailRepositorySpy } from "@/data/mocks";
 
 import { DbAuthentication } from "./DbAuthentication";
 
+type SutTypes = {
+    sut: DbAuthentication;
+    loadUserByEmailRepositorySpy: LoadUserByEmailRepositorySpy;
+};
+
+const makeSut = (): SutTypes => {
+    const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy();
+    const sut = new DbAuthentication(loadUserByEmailRepositorySpy);
+
+    return { sut, loadUserByEmailRepositorySpy };
+};
+
 describe("DbAuthentication", () => {
     it("should call LoadUserByEmailRepository with correct email", async () => {
+        const { sut, loadUserByEmailRepositorySpy } = makeSut();
         const email = faker.internet.email();
-        const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy();
-        const sut = new DbAuthentication(loadUserByEmailRepositorySpy);
 
         await sut.auth({
             email,
@@ -16,5 +27,18 @@ describe("DbAuthentication", () => {
         });
 
         expect(loadUserByEmailRepositorySpy.email).toBe(email);
+    });
+
+    it("should return null if LoadUserByEmailRepository returns null", async () => {
+        const { sut, loadUserByEmailRepositorySpy } = makeSut();
+        const email = faker.internet.email();
+        loadUserByEmailRepositorySpy.result = null;
+
+        const token = await sut.auth({
+            email,
+            password: faker.internet.password(),
+        });
+
+        expect(token).toBeNull();
     });
 });
