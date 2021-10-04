e8553384139d9ae545883725a52740200ef92cbf
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-10-04 21:32:59 +0200

    test: ensure DbAuthentication calls HashComparer with correct values

--- src/data/mocks/mockCryptography.ts
+++ src/data/mocks/mockCryptography.ts
@@ -1,6 +1,6 @@
 import faker from "faker";
 
-import { Hasher } from "@/data/protocols/cryptography";
+import { HashComparer, Hasher } from "@/data/protocols/cryptography";
 
 export class HasherSpy implements Hasher {
     digest = faker.datatype.uuid();
@@ -9,5 +9,17 @@ export class HasherSpy implements Hasher {
     async hash(plaintext: string): Promise<string> {
         this.plaintext = plaintext;
         return this.digest;
+    }
+}
+
+export class HashComparerSpy implements HashComparer {
+    plaintext: string;
+    digest: string;
+    isValid = true;
+
+    async compare(plaintext: string, digest: string): Promise<boolean> {
+        this.plaintext = plaintext;
+        this.digest = digest;
+        return this.isValid;
     }
 }
--- src/data/usecases/Authentication/DbAuthentication.spec.ts
+++ src/data/usecases/Authentication/DbAuthentication.spec.ts
@@ -1,4 +1,4 @@
-import { LoadUserByEmailRepositorySpy } from "@/data/mocks";
+import { HashComparerSpy, LoadUserByEmailRepositorySpy } from "@/data/mocks";
 import { mockAuthenticationParams, throwError } from "@/domain/mocks";
 
 import { DbAuthentication } from "./DbAuthentication";
@@ -6,13 +6,18 @@ import { DbAuthentication } from "./DbAuthentication";
 type SutTypes = {
     sut: DbAuthentication;
     loadUserByEmailRepositorySpy: LoadUserByEmailRepositorySpy;
+    hashComparerSpy: HashComparerSpy;
 };
 
 const makeSut = (): SutTypes => {
     const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy();
-    const sut = new DbAuthentication(loadUserByEmailRepositorySpy);
+    const hashComparerSpy = new HashComparerSpy();
+    const sut = new DbAuthentication(
+        loadUserByEmailRepositorySpy,
+        hashComparerSpy,
+    );
 
-    return { sut, loadUserByEmailRepositorySpy };
+    return { sut, loadUserByEmailRepositorySpy, hashComparerSpy };
 };
 
 describe("DbAuthentication", () => {
@@ -40,5 +45,16 @@ describe("DbAuthentication", () => {
         ).mockImplementation(throwError);
         const promise = sut.auth(mockAuthenticationParams());
         await expect(promise).rejects.toThrow();
+    });
+
+    it("should call HashComparer with correct values", async () => {
+        const { sut, hashComparerSpy, loadUserByEmailRepositorySpy } =
+            makeSut();
+        const authenticationParams = mockAuthenticationParams();
+        await sut.auth(authenticationParams);
+        expect(hashComparerSpy.plaintext).toBe(authenticationParams.password);
+        expect(hashComparerSpy.digest).toBe(
+            loadUserByEmailRepositorySpy.result?.password,
+        );
     });
 });
