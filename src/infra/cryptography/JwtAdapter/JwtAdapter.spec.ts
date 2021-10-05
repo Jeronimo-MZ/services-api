e3e60bf57399ef86edb311a10c33a32435909aa2
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-10-05 19:46:14 +0200

    test: ensure JwtAdapter throws if sign throws

--- src/infra/cryptography/JwtAdapter/JwtAdapter.spec.ts
+++ src/infra/cryptography/JwtAdapter/JwtAdapter.spec.ts
@@ -1,6 +1,8 @@
 import faker from "faker";
 import jwt from "jsonwebtoken";
 
+import { throwError } from "@/domain/mocks";
+
 import { JwtAdapter } from "./JwtAdapter";
 
 jest.mock("jsonwebtoken", () => ({
@@ -10,6 +12,7 @@ jest.mock("jsonwebtoken", () => ({
 }));
 
 const secret = faker.random.alphaNumeric(50);
+const plaintext = faker.datatype.uuid();
 
 const makeSut = (): JwtAdapter => {
     return new JwtAdapter(secret);
@@ -19,7 +22,6 @@ describe("JwtAdapter", () => {
     describe("encrypt()", () => {
         it("should calls sign with correct values", async () => {
             const sut = makeSut();
-            const plaintext = faker.datatype.uuid();
             const signSpy = jest.spyOn(jwt, "sign");
             await sut.encrypt(plaintext);
             expect(signSpy).toHaveBeenCalledWith({ data: plaintext }, secret, {
@@ -29,9 +31,15 @@ describe("JwtAdapter", () => {
 
         it("should return a token on sign success", async () => {
             const sut = makeSut();
-            const plaintext = faker.datatype.uuid();
             const accessToken = await sut.encrypt(plaintext);
             expect(accessToken).toBe("any_token");
+        });
+
+        it("should throw if sign throws", async () => {
+            const sut = makeSut();
+            jest.spyOn(jwt, "sign").mockImplementationOnce(throwError);
+            const promise = sut.encrypt(plaintext);
+            await expect(promise).rejects.toThrow();
         });
     });
 });
