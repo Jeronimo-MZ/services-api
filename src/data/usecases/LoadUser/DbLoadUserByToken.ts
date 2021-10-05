698afeeda74fdf48c4cd12668b59eaa25b5e8e53
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-10-06 00:28:50 +0200

    feat: ensure DbLoadUserByToken calls Decrypter with correct value

--- src/data/usecases/LoadUser/DbLoadUserByToken.ts
+++ src/data/usecases/LoadUser/DbLoadUserByToken.ts
@@ -1,3 +1,4 @@
+import { Decrypter } from "@/data/protocols/cryptography/Decrypter";
 import { LoadUserByTokenRepository } from "@/data/protocols/database/User";
 import { User } from "@/domain/models/User";
 import { LoadUserByToken } from "@/domain/usecases/LoadUserByToken";
@@ -5,8 +6,10 @@ import { LoadUserByToken } from "@/domain/usecases/LoadUserByToken";
 export class DbLoadUserByToken implements LoadUserByToken {
     constructor(
         private readonly loadUserByTokenRepository: LoadUserByTokenRepository,
+        private readonly decrypter: Decrypter,
     ) {}
     async load(accessToken: string): Promise<User | null> {
+        await this.decrypter.decrypt(accessToken);
         await this.loadUserByTokenRepository.loadByToken(accessToken);
         return null;
     }
