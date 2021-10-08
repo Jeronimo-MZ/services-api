7daeffc3368359d840a34a9ef8195439b9d61521
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-10-08 16:36:40 +0200

    test: ensure AddCustomerController returns 400 if Validation fails

--- src/presentation/controllers/Customer/AddCustomer/AddCustomerController.spec.ts
+++ src/presentation/controllers/Customer/AddCustomer/AddCustomerController.spec.ts
@@ -1,5 +1,7 @@
 import faker from "faker";
 
+import { InvalidParamError } from "@/presentation/errors";
+import { badRequest } from "@/presentation/helpers/http/httpHelper";
 import { ValidationSpy } from "@/presentation/mocks/mockValidation";
 
 import { AddCustomerController } from "./AddCustomerController";
@@ -31,5 +33,12 @@ describe("SignUp Controller", () => {
         const request = mockRequest();
         await sut.handle(request);
         expect(validationSpy.input).toEqual(request);
+    });
+
+    it("should return 400 if validation fails", async () => {
+        const { sut, validationSpy } = makeSut();
+        validationSpy.error = new InvalidParamError(faker.random.word());
+        const httpResponse = await sut.handle(mockRequest());
+        expect(httpResponse).toEqual(badRequest(validationSpy.error));
     });
 });
