6be055cf386548b61356e9c6cf55c5b8c6a20c42
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-10-08 17:59:15 +0200

    feat: ensure AuthMiddleware returns 200 if LoadUserByToken returns a user

--- src/presentation/middlewares/AuthMiddleware.ts
+++ src/presentation/middlewares/AuthMiddleware.ts
@@ -1,16 +1,22 @@
 import { LoadUserByToken } from "@/domain/usecases/LoadUserByToken";
-
-import { AccessDeniedError } from "../errors/AccessDeniedError";
-import { forbidden, serverError } from "../helpers/http/httpHelper";
-import { HttpResponse } from "../protocols";
-import { Middleware } from "../protocols/middleware";
+import { AccessDeniedError } from "@/presentation/errors/AccessDeniedError";
+import {
+    forbidden,
+    ok,
+    serverError,
+} from "@/presentation/helpers/http/httpHelper";
+import { HttpResponse } from "@/presentation/protocols";
+import { Middleware } from "@/presentation/protocols/middleware";
 
 export class AuthMiddleware implements Middleware<AuthMiddleware.Request> {
     constructor(private readonly loadUserByToken: LoadUserByToken) {}
     async handle(request: AuthMiddleware.Request): Promise<HttpResponse> {
         try {
             const { accessToken } = request;
-            if (accessToken) await this.loadUserByToken.load(accessToken);
+            if (accessToken) {
+                const user = await this.loadUserByToken.load(accessToken);
+                if (user) return ok({ userId: user.id });
+            }
             return forbidden(new AccessDeniedError());
         } catch (error) {
             return serverError(error as Error);
