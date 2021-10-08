2a84560f5857b4824f685638d86d6a83be5a05f5
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-10-08 16:56:45 +0200

    feat: ensure AddCustomerController calls AddCustomer with correct values

--- src/presentation/controllers/Customer/AddCustomer/AddCustomerController.ts
+++ src/presentation/controllers/Customer/AddCustomer/AddCustomerController.ts
@@ -1,3 +1,4 @@
+import { AddCustomer } from "@/domain/usecases/AddCustomer";
 import {
     badRequest,
     serverError,
@@ -7,7 +8,10 @@ import { Controller, HttpResponse, Validation } from "@/presentation/protocols";
 export class AddCustomerController
     implements Controller<AddCustomerController.Request>
 {
-    constructor(private readonly validation: Validation) {}
+    constructor(
+        private readonly validation: Validation,
+        private readonly addCustomer: AddCustomer,
+    ) {}
 
     async handle(
         request: AddCustomerController.Request,
@@ -15,6 +19,12 @@ export class AddCustomerController
         try {
             const error = this.validation.validate(request);
             if (error) return badRequest(error);
+            const { institution, name, userId } = request;
+            await this.addCustomer.add({
+                institution,
+                name,
+                providerId: userId,
+            });
             return undefined as any;
         } catch (error) {
             return serverError(error as Error);
