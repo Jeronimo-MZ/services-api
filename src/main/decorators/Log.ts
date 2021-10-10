0617cde1c896ddefe74778c8bbc07f3e655e08fa
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-10-10 18:51:33 +0200

    feat: ensure LogControllerDecorator calls LogErrorRepository with correct error

--- src/main/decorators/Log.ts
+++ src/main/decorators/Log.ts
@@ -1,9 +1,19 @@
+import { LogErrorRepository } from "@/data/protocols/database/Log";
 import { Controller, HttpResponse } from "@/presentation/protocols";
 
 export class LogControllerDecorator implements Controller {
-    constructor(private readonly controller: Controller) {}
+    constructor(
+        private readonly controller: Controller,
+        private readonly logErrorRepository: LogErrorRepository,
+    ) {}
 
     async handle(httpRequest: any): Promise<HttpResponse> {
-        return await this.controller.handle(httpRequest);
+        const response = await this.controller.handle(httpRequest);
+
+        if (response.statusCode === 500) {
+            this.logErrorRepository.logError(response.body.stack);
+        }
+
+        return response;
     }
 }
