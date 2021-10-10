c835dd734f8767fece25ba459c2557d068f736f4
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-10-10 18:52:44 +0200

    test: ensure LogControllerDecorator calls LogErrorRepository with correct error

--- src/main/decorators/Log.spec.ts
+++ src/main/decorators/Log.spec.ts
@@ -1,7 +1,8 @@
 import faker from "faker";
 
+import { LogErrorRepositorySpy } from "@/data/mocks/mockDbLog";
 import { LogControllerDecorator } from "@/main/decorators/Log";
-import { ok } from "@/presentation/helpers/http/httpHelper";
+import { ok, serverError } from "@/presentation/helpers/http/httpHelper";
 import { Controller, HttpResponse } from "@/presentation/protocols";
 
 class ControllerSpy implements Controller {
@@ -13,18 +14,29 @@ class ControllerSpy implements Controller {
         return this.httpResponse;
     }
 }
+const mockServerError = (): HttpResponse => {
+    const fakeError = new Error();
+    fakeError.stack = "any_stack";
+    return serverError(fakeError);
+};
 
 type SutTypes = {
     sut: LogControllerDecorator;
     controllerSpy: ControllerSpy;
+    logErrorRepositorySpy: LogErrorRepositorySpy;
 };
 
 const makeSut = (): SutTypes => {
     const controllerSpy = new ControllerSpy();
-    const sut = new LogControllerDecorator(controllerSpy);
+    const logErrorRepositorySpy = new LogErrorRepositorySpy();
+    const sut = new LogControllerDecorator(
+        controllerSpy,
+        logErrorRepositorySpy,
+    );
     return {
         sut,
         controllerSpy,
+        logErrorRepositorySpy,
     };
 };
 
@@ -40,5 +52,13 @@ describe("LogController Decorator", () => {
         const { sut, controllerSpy } = makeSut();
         const httpResponse = await sut.handle(faker.lorem.sentence());
         expect(httpResponse).toEqual(controllerSpy.httpResponse);
+    });
+
+    it("should call LogErrorRepository with correct error if controller returns a server error", async () => {
+        const { sut, controllerSpy, logErrorRepositorySpy } = makeSut();
+        const serverError = mockServerError();
+        controllerSpy.httpResponse = serverError;
+        await sut.handle(faker.random.objectElement());
+        expect(logErrorRepositorySpy.stack).toBe(serverError.body.stack);
     });
 });
