c830b46f34320f0a3d77eb928b683b7f926be245
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-10-10 16:59:29 +0200

    test: ensure CustomerMongoRepository returns an array of customers with given providedId

--- src/infra/database/mongodb/repositories/Customer/Customer.spec.ts
+++ src/infra/database/mongodb/repositories/Customer/Customer.spec.ts
@@ -1,3 +1,6 @@
+import bcrypt from "bcrypt";
+import { ObjectID } from "bson";
+import faker from "faker";
 import { Collection } from "mongodb";
 
 import { mockAddCustomerParams } from "@/domain/mocks";
@@ -7,22 +10,53 @@ import { CollectionNames, MongoHelper } from "@/infra/database/mongodb/helpers";
 import { CustomerMongoRepository } from "./Customer";
 
 const makeSut = (): CustomerMongoRepository => new CustomerMongoRepository();
+let customersCollection: Collection;
+let usersCollection: Collection;
+
+const makeUserId = async () => {
+    const userData = {
+        email: faker.internet.email(),
+        name: faker.name.findName(),
+        password: faker.internet.password(),
+    };
+
+    const { insertedId } = await usersCollection.insertOne({
+        ...userData,
+        password: await bcrypt.hash(userData.password, 12),
+    });
+
+    return {
+        id: insertedId.toHexString(),
+    };
+};
+
+const makeCustomer = async (providerId: string): Promise<Customer> => {
+    const customerData = {
+        institution: faker.company.companyName(),
+        name: faker.name.findName(),
+        providerId: new ObjectID(providerId),
+        phone: null,
+    };
+
+    await customersCollection.insertOne(customerData);
+    return MongoHelper.map(customerData);
+};
 
 describe("Customer Mongo Repository", () => {
-    let customersCollection: Collection;
-
     beforeAll(async () => {
         await MongoHelper.connect(process.env.MONGO_URL as string);
+        customersCollection = await MongoHelper.getCollection(
+            CollectionNames.CUSTOMER,
+        );
+        usersCollection = await MongoHelper.getCollection(CollectionNames.USER);
     });
     afterAll(async () => {
         await MongoHelper.disconnect();
     });
 
     beforeEach(async () => {
-        customersCollection = await MongoHelper.getCollection(
-            CollectionNames.CUSTOMER,
-        );
         await customersCollection.deleteMany({});
+        await usersCollection.deleteMany({});
     });
 
     describe("add()", () => {
@@ -37,6 +71,30 @@ describe("Customer Mongo Repository", () => {
             expect(customer.institution).toBe(addCustomerParams.institution);
             expect(customer.name).toBe(addCustomerParams.name);
             expect(customer.phone).toBeNull();
+        });
+    });
+
+    describe("loadByProviderId()", () => {
+        it("should returns an array of customers with the given providerId", async () => {
+            const userIds = [await makeUserId(), await makeUserId()].map(
+                ({ id }) => id,
+            );
+            const customers: Record<string, Customer[]> = {};
+
+            for (const id of userIds) {
+                customers[id] = [
+                    await makeCustomer(id),
+                    await makeCustomer(id),
+                    await makeCustomer(id),
+                ];
+            }
+
+            userIds.push((await makeUserId()).id);
+            const sut = makeSut();
+            let loadedCustomers = await sut.loadByProviderId(userIds[1]);
+            expect(loadedCustomers).toEqual(customers[userIds[1]]);
+            loadedCustomers = await sut.loadByProviderId(userIds[2]);
+            expect(loadedCustomers).toEqual([]);
         });
     });
 });
