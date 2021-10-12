56ee1414004600cf5f83d9b398c097e4d1c7a5bf
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-10-12 23:54:15 +0200

    feat: ensure NotRequiredFieldValidation calls Validation if field is provided

--- src/validation/validators/NotRequiredFieldValidation.ts
+++ src/validation/validators/NotRequiredFieldValidation.ts
@@ -1,9 +1,15 @@
 import { Validation } from "@/presentation/protocols";
 
 export class NotRequiredFieldValidation implements Validation {
-    constructor(private fieldName: string) {}
+    constructor(
+        private fieldName: string,
+        private readonly validation: Validation,
+    ) {}
 
-    validate(_input: any): Error | null {
+    validate(input: any): Error | null {
+        if (input[this.fieldName]) {
+            this.validation.validate(input);
+        }
         return null;
     }
 }
