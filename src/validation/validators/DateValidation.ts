d7b26fd4799c800de89e564557d7c9ddf1287483
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-10-13 00:14:04 +0200

    feat: ensure DateValidation calls DateValidator with correct date

---
+++ src/validation/validators/DateValidation.ts
@@ -0,0 +1,14 @@
+import { Validation } from "@/presentation/protocols";
+import { DateValidator } from "@/validation/protocols/DateValidator";
+
+export class DateValidation implements Validation {
+    constructor(
+        private readonly fieldName: string,
+        private readonly dateValidator: DateValidator,
+    ) {}
+
+    validate(input: any): Error | null {
+        this.dateValidator.isValid(input[this.fieldName]);
+        return undefined as any;
+    }
+}
