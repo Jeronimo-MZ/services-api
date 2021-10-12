1942b5707a8911feedf1e44ccf9fefa10726ade2
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-10-12 23:54:46 +0200

    test: ensure NotRequiredFieldValidation calls Validation if field is provided

--- src/validation/validators/NotRequiredFieldValidation.spec.ts
+++ src/validation/validators/NotRequiredFieldValidation.spec.ts
@@ -1,17 +1,34 @@
 import faker from "faker";
 
+import { ValidationSpy } from "@/presentation/mocks/mockValidation";
+
 import { NotRequiredFieldValidation } from "./NotRequiredFieldValidation";
 
 const field = faker.random.word();
-
-const makeSut = (): NotRequiredFieldValidation => {
-    return new NotRequiredFieldValidation(field);
+type SutTypes = {
+    sut: NotRequiredFieldValidation;
+    validationSpy: ValidationSpy;
+};
+const makeSut = (): SutTypes => {
+    const validationSpy = new ValidationSpy();
+    const sut = new NotRequiredFieldValidation(field, validationSpy);
+    return {
+        sut,
+        validationSpy,
+    };
 };
 
 describe("NotRequiredField Validation", () => {
     it("should return null if field is not provided", () => {
-        const sut = makeSut();
+        const { sut } = makeSut();
         const error = sut.validate({});
         expect(error).toBeNull();
+    });
+
+    it("should call Validation if field is provided", () => {
+        const { sut, validationSpy } = makeSut();
+        const input = { [field]: faker.random.word() };
+        sut.validate(input);
+        expect(validationSpy.input).toEqual(input);
     });
 });
