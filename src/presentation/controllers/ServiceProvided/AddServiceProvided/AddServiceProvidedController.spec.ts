b81fa0f9d963cb59993481497bd7542562c3a6b9
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-10-12 23:06:34 +0200

    test: ensure AddServiceProvidedController throws if Validation throws

--- src/presentation/controllers/ServiceProvided/AddServiceProvided/AddServiceProvidedController.spec.ts
+++ src/presentation/controllers/ServiceProvided/AddServiceProvided/AddServiceProvidedController.spec.ts
@@ -1,5 +1,7 @@
 import faker from "faker";
 
+import { InvalidParamError } from "@/presentation/errors";
+import { badRequest } from "@/presentation/helpers/http/httpHelper";
 import { ValidationSpy } from "@/presentation/mocks/mockValidation";
 
 import { AddServiceProvidedController } from "./AddServiceProvidedController";
@@ -34,5 +36,12 @@ describe("SignUp Controller", () => {
         const request = mockRequest();
         await sut.handle(request);
         expect(validationSpy.input).toEqual(request);
+    });
+
+    it("should return 400 if validation fails", async () => {
+        const { sut, validationSpy } = makeSut();
+        validationSpy.error = new InvalidParamError(faker.random.word());
+        const httpResponse = await sut.handle(mockRequest());
+        expect(httpResponse).toEqual(badRequest(validationSpy.error));
     });
 });
