1a87443abd20453e53331f34f6487d9abf8d4e62
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-10-17 02:41:25 +0200

    test: ensure NoCache middleware disables cache

---
+++ src/main/middlewares/noCache.spec.ts
@@ -0,0 +1,24 @@
+import request from "supertest";
+
+import { app } from "@/main/config/app";
+
+import { noCache } from "./noCache";
+
+describe("NoCache middleware", () => {
+    it("should disable cache", async () => {
+        app.get("/test_no_cache", noCache, (_request, response) => {
+            return response.send();
+        });
+
+        await request(app)
+            .get("/test_no_cache")
+            .send()
+            .expect(
+                "cache-control",
+                "no-store, no-cache, must-revalidate, proxy-revalidate",
+            )
+            .expect("pragma", "no-cache")
+            .expect("expires", "0")
+            .expect("surrogate-control", "no-store");
+    });
+});
