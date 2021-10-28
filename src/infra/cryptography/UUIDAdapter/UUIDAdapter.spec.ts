00c35d68c6d9ac0f3e1c9c435aba9e159bd3c481
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-10-29 00:37:03 +0200

    test: ensure UUIDAdapter calls uuid.v4

---
+++ src/infra/cryptography/UUIDAdapter/UUIDAdapter.spec.ts
@@ -0,0 +1,19 @@
+import faker from "faker";
+import { mocked } from "ts-jest/utils";
+import { v4 } from "uuid";
+
+import { UUIDAdapter } from "./UUIDAdapter";
+
+const generatedUUID = faker.datatype.uuid();
+jest.mock("uuid");
+mocked(v4).mockReturnValue(generatedUUID);
+
+const makeSut = () => new UUIDAdapter();
+
+describe("UUIDAdapter", () => {
+    it("should call uuid.v4", () => {
+        const sut = makeSut();
+        sut.generate();
+        expect(v4).toHaveBeenCalledTimes(1);
+    });
+});
