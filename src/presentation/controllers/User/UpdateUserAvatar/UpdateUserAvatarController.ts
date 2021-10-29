a60b191f82a6c650920ba6add7a548e0a20337c4
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-10-29 15:48:50 +0200

    feat: ensure UpdateUserAvatarController returns 400 if validation fails

--- src/presentation/controllers/User/UpdateUserAvatar/UpdateUserAvatarController.ts
+++ src/presentation/controllers/User/UpdateUserAvatar/UpdateUserAvatarController.ts
@@ -1,11 +1,17 @@
+import { badRequest } from "@/presentation/helpers/http/httpHelper";
 import { Controller, HttpResponse, Validation } from "@/presentation/protocols";
 
 export class UpdateUserAvatarController
     implements Controller<UpdateUserAvatarController.Request>
 {
     constructor(private readonly validation: Validation) {}
-    handle(request: UpdateUserAvatarController.Request): Promise<HttpResponse> {
-        this.validation.validate(request);
+    async handle(
+        request: UpdateUserAvatarController.Request,
+    ): Promise<HttpResponse> {
+        const error = this.validation.validate(request);
+        if (error) {
+            return badRequest(error);
+        }
         return undefined as any;
     }
 }
