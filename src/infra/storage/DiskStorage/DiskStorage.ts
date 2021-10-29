c733586b1b8892e2023795594ba0d2e5c2b82c81
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-10-29 11:21:31 +0200

    feat: ensure DiskStorage calls unlink with correct values

--- src/infra/storage/DiskStorage/DiskStorage.ts
+++ src/infra/storage/DiskStorage/DiskStorage.ts
@@ -1,15 +1,20 @@
-import { writeFile } from "fs/promises";
+import { unlink, writeFile } from "fs/promises";
 import path from "path";
 
-import { SaveFile } from "@/data/protocols/storage";
+import { DeleteFile, SaveFile } from "@/data/protocols/storage";
 
-export class DiskStorage implements SaveFile {
+export class DiskStorage implements SaveFile, DeleteFile {
     constructor(private readonly staticFilesDirectory: string) {}
+
     async save({ file, fileName }: SaveFile.Input): Promise<string> {
         await writeFile(
             path.resolve(this.staticFilesDirectory, fileName),
             file,
         );
         return fileName;
+    }
+
+    async delete({ fileName }: DeleteFile.Input): Promise<void> {
+        await unlink(path.resolve(this.staticFilesDirectory, fileName));
     }
 }
