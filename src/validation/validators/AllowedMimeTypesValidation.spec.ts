18b03d5f4bedda45cd1ab595f88600cb3802f89c
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-10-29 12:38:11 +0200

    test: ensure AllowedMimeTypesValidation returns null if validation succeeds

---
+++ src/validation/validators/AllowedMimeTypesValidation.spec.ts
@@ -0,0 +1,25 @@
+import faker from "faker";
+
+import { AllowedMimeTypesValidation } from "./AllowedMimeTypesValidation";
+
+const field = faker.random.word();
+
+describe("AllowedMimeType Validation", () => {
+    it("should return null if value is valid", () => {
+        const sut = new AllowedMimeTypesValidation(["png"], field);
+        const error = sut.validate({ [field]: "image/png" });
+        expect(error).toBeNull();
+    });
+
+    it("should return null if value is valid", () => {
+        const sut = new AllowedMimeTypesValidation(["jpg"], field);
+        const error = sut.validate({ [field]: "image/jpg" });
+        expect(error).toBeNull();
+    });
+
+    it("should return null if value is valid", () => {
+        const sut = new AllowedMimeTypesValidation(["jpg"], field);
+        const error = sut.validate({ [field]: "image/jpeg" });
+        expect(error).toBeNull();
+    });
+});
