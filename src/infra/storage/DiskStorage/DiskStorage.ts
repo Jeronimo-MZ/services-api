308da9c31ae7f252fe50f9b97d5d5d3ba551d881
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-10-29 10:53:20 +0200

    feat: ensure DiskStorage calls writeFile with correct values

---
+++ src/infra/storage/DiskStorage/DiskStorage.ts
@@ -0,0 +1,15 @@
+import { writeFile } from "fs/promises";
+import path from "path";
+
+import { SaveFile } from "@/data/protocols/storage";
+
+export class DiskStorage implements SaveFile {
+    constructor(private readonly staticFilesDirectory: string) {}
+    async save({ file, fileName }: SaveFile.Input): Promise<string> {
+        await writeFile(
+            path.resolve(this.staticFilesDirectory, fileName),
+            file,
+        );
+        return undefined as any;
+    }
+}
