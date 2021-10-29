73c526ca2553bcc7d4ccfc112dd2131a0bee586f
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-10-29 12:13:28 +0200

    test: ensure MaxFileSizeValidation returns null if validation succeeds

--- src/validation/validators/MaxFileSizeValidation.spec.ts
+++ src/validation/validators/MaxFileSizeValidation.spec.ts
@@ -8,15 +8,22 @@ type SutTypes = {
     sut: MaxFileSizeValidation;
 };
 const field = faker.random.word();
+const maxSizeInMb = 5;
 
 const makeSut = (): SutTypes => {
-    const maxSizeInMb = 5;
     const sut = new MaxFileSizeValidation(maxSizeInMb, field);
     return {
         sut,
     };
 };
 describe("MaxFileSize Validation", () => {
+    it("should return null if validation succeeds", () => {
+        const { sut } = makeSut();
+        const buffer = Buffer.from(new ArrayBuffer(4 * 1024 * 1024));
+        const error = sut.validate({ [field]: buffer });
+        expect(error).toBeNull();
+    });
+
     it("should return InvalidParamError if field is not a Buffer", () => {
         const { sut } = makeSut();
         const error = sut.validate({ [field]: faker.datatype.number() });
