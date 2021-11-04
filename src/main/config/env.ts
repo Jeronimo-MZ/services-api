4833a0235aca14f66f3beee27b9113f2238b4c07
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-11-04 22:52:07 +0200

    refactor: rename static files folder

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -3,5 +3,5 @@ import path from "path";
 export const env = {
     mongoUrl: process.env.MONGO_URL || "mongodb://localhost:27017/services-api",
     secret: process.env.SECRET || "dev-secret",
-    staticFilesPath: path.resolve(__dirname, "../../../static"),
+    staticFilesPath: path.resolve(__dirname, "../../../public"),
 };
