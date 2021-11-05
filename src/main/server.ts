b60db7c672538a921ae0238856555c1a48f19bf9
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-11-05 12:01:16 +0200

    feat: add unhandledRejection and uncaught exception handlers

--- src/main/server.ts
+++ src/main/server.ts
@@ -16,6 +16,18 @@ enum ExitStatus {
 const PORT = process.env.PORT || 3333;
 const exitSignals: NodeJS.Signals[] = ["SIGINT", "SIGTERM", "SIGQUIT"];
 
+process.on("unhandledRejection", (reason, promise) => {
+    pinoLogger.error(
+        `App exiting due to an unhandled promise: ${promise} and reason: ${reason}`,
+    );
+    throw reason; // will be handled by uncaughtException handler
+});
+
+process.on("uncaughtException", error => {
+    pinoLogger.error(`App exiting due to an uncaught exception: ${error}`);
+    process.exit(ExitStatus.Failure);
+});
+
 setupLogger(app);
 setupRoutes(app);
 
